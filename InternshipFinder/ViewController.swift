e45851e8927d70de7cc957b498bceb7530f81c49
GG <gabrielleseibert@gmail.com>
2022-06-16 16:58:31 -0400

    added buttons but they dont work yet

--- InternshipFinder/ViewController.swift
+++ InternshipFinder/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func CLAG(_ sender: Any) {
+        let googleURL = URL(string:
+            "https://www.codelikeagirl.com/internships/apply/?ja-job=1368303")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func STEAMBoat(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://thesteamboat.godaddysites.com")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func TGCWebsite(_ sender: Any) {
+        let googleURL = URL(string:
+        "http://thegirlcode.co/joinus")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func TGCAppForm(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdNnrbYFbdkTMnlod-Pp74KRPLipqN2RHmT1gwDoudFcxbG9Q/viewform")
+            UIApplication.shared.open(googleURL!)
+    }
+    
+    
 }
 
