563f619941bd38a52545e5a45544659bb1c4185a
GG <gabrielleseibert@gmail.com>
2022-06-16 23:28:48 -0400

    added the internships

--- InternshipFinder/TableViewController2ViewController.swift
+++ InternshipFinder/TableViewController2ViewController.swift
@@ -25,5 +25,56 @@ class TableViewController2ViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    @IBAction func STEAMBoatWeb(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://thesteamboat.godaddysites.com")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func TGCWeb(_ sender: Any) {
+        let googleURL = URL(string:
+        "http://thegirlcode.co/joinus")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func TGCAppForm(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdNnrbYFbdkTMnlod-Pp74KRPLipqN2RHmT1gwDoudFcxbG9Q/viewform")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func CLAGWeb(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://www.codelikeagirl.com/internships/apply/?ja-job=1368303")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func QIWeb(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://qworld.net/qintern-2022/")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func cherieWeb(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://sites.google.com/view/cheriemagazine/home?authuser=0")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func cherieAppForm(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://docs.google.com/forms/d/e/1FAIpQLSewvkYAH4uPIED_aLAR-UDR2dTVRjOYT_1X-IFb7Ld7L7S3Pw/viewform")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    @IBAction func citronTalkWeb(_ sender: Any) {
+        let googleURL = URL(string:
+        "https://citrontalk.com/about/")
+        UIApplication.shared.open(googleURL!)
+    }
+    
+    
 
 }
--- InternshipFinder/ViewController.swift
+++ InternshipFinder/ViewController.swift
@@ -6,37 +6,12 @@
 //
 
 import UIKit
-import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func CLAG(_ sender: Any) {
-        let googleURL = URL(string:
-            "https://www.codelikeagirl.com/internships/apply/?ja-job=1368303")
-        UIApplication.shared.open(googleURL!)
-    }
-    
-    @IBAction func STEAMBoat(_ sender: Any) {
-        let googleURL = URL(string:
-        "https://thesteamboat.godaddysites.com")
-        UIApplication.shared.open(googleURL!)
-    }
-    
-    @IBAction func TGCWebsite(_ sender: Any) {
-        let googleURL = URL(string:
-        "http://thegirlcode.co/joinus")
-        UIApplication.shared.open(googleURL!)
-    }
-    
-    @IBAction func TGCAppForm(_ sender: Any) {
-        let googleURL = URL(string:
-        "https://docs.google.com/forms/d/e/1FAIpQLSdNnrbYFbdkTMnlod-Pp74KRPLipqN2RHmT1gwDoudFcxbG9Q/viewform")
-            UIApplication.shared.open(googleURL!)
     }
     
     
